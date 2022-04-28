a739c144d30584238dba67ecf31f2ac589fb714b
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-04-28 16:56:46 -0300

    Added class to zipkin auto configuration

---
+++ servicethree/src/main/java/com/huntercodexs/zipkintracingdemo/servicethree/config/ZipkinAutoConfiguration.java
@@ -0,0 +1,22 @@
+package com.huntercodexs.zipkintracingdemo.servicethree.config;
+
+import brave.sampler.Sampler;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableAutoConfiguration
+public class ZipkinAutoConfiguration {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Sampler alwaysSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
