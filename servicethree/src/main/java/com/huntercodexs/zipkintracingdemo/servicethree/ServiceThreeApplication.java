48ae3b161de6f15814a378453e9f612887793a38
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-05-01 22:28:48 -0300

    Update Zipkin traceId

--- servicethree/src/main/java/com/huntercodexs/zipkintracingdemo/servicethree/ServiceThreeApplication.java
+++ servicethree/src/main/java/com/huntercodexs/zipkintracingdemo/servicethree/ServiceThreeApplication.java
@@ -1,12 +1,15 @@
 package com.huntercodexs.zipkintracingdemo.servicethree;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ServiceThreeApplication {
 
 	public static void main(String[] args) {
+		log.debug("SERVICE-THREE START");
 		SpringApplication.run(ServiceThreeApplication.class, args);
 	}
 
--- servicethree/src/main/java/com/huntercodexs/zipkintracingdemo/servicethree/restcontroller/UserController.java
+++ servicethree/src/main/java/com/huntercodexs/zipkintracingdemo/servicethree/restcontroller/UserController.java
@@ -5,15 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import java.util.logging.Logger;
 
+@Slf4j
 @ControllerAdvice
 @RestController
 @RequestMapping("${api.prefix}")
 @CrossOrigin(origins = "*")
 public class UserController {
 
-    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+    //private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     ZipkinFlowService zipkinFlowService;
@@ -25,6 +30,7 @@ public class UserController {
     public ResponseEntity<?> start() {
         System.out.println("ServiceThree - zipkinFlowStart");
         LOG.info("ServiceThree - zipkinFlowStart");
+        log.info("ServiceThree - zipkinFlowStart @Slf4j");
         return zipkinFlowService.start();
     }
 
@@ -35,6 +41,7 @@ public class UserController {
     public ResponseEntity<?> finish() {
         System.out.println("ServiceThree - zipkinFlowFinish");
         LOG.info("ServiceThree - zipkinFlowFinish");
+        log.info("ServiceThree - zipkinFlowFinish @Slf4j");
         return zipkinFlowService.finish();
     }
 
